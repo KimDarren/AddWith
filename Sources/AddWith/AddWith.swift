91692da096b354c674cc667781821d0fee7121fa
KimDarren <korean.darren@gmail.com>
2017-07-03 11:36:01 +0900

    add source file

---
+++ Sources/AddWith/AddWith.swift
@@ -0,0 +1,78 @@
+//  MIT License
+//
+//  Copyright (c) 2017 KIM TAE JUN (korean.darren@gmail.com)
+//
+//  Permission is hereby granted, free of charge, to any person obtaining a copy
+//  of this software and associated documentation files (the "Software"), to deal
+//  in the Software without restriction, including without limitation the rights
+//  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+//  copies of the Software, and to permit persons to whom the Software is
+//  furnished to do so, subject to the following conditions:
+//
+//  The above copyright notice and this permission notice shall be included in all
+//  copies or substantial portions of the Software.
+//
+//  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+//  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+//  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+//  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+//  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+//  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+//  SOFTWARE.
+
+import UIKit
+
+public protocol AddWith {}
+
+extension AddWith where Self: UIView {
+  
+  /// Add views to the end of the receiver’s list of subviews.
+  /// This method establishes a strong reference to view and sets its next responder to the receiver, which is its new superview.
+  /// Views can have only one superview. If view already has a superview and that view is not the receiver, this method removes the previous superview before making the receiver its new superview.
+  ///
+  ///     self.view.add(
+  ///       self.titleLabel,
+  ///       self.descriptionLabel,
+  ///       self.imageView
+  ///     }
+  ///
+  /// - Parameter subviews: The collection of views to be added. After being added, this views appear on top of any other subviews.
+  
+  public func add(_ subviews: [UIView]) {
+    subviews.forEach {
+      self.addSubview($0)
+    }
+  }
+  
+  
+  /// Add views to the end of the receiver’s list of subviews.
+  /// This method establishes a strong reference to view and sets its next responder to the receiver, which is its new superview.
+  /// Views can have only one superview. If view already has a superview and that view is not the receiver, this method removes the previous superview before making the receiver its new superview.
+  ///
+  /// - Parameter subviews: The collection of views to be added. After being added, this views appear on top of any other subviews.
+  
+  public func add(_ subviews: UIView...) {
+    self.add(subviews)
+  }
+  
+  
+  /// Add views to the end of the receiver's list of subviews, and return the receiver.
+  ///
+  ///     self.scrollView.add(
+  ///       self.contentContainer.with(
+  ///         self.descriptionLabel
+  ///         self.imageView
+  ///       }
+  ///     }
+  ///
+  /// - Parameter subviews: The collection of views to be added. After being added, this views appear on top of any other subviews.
+  /// - Returns: Receiver, with new subviews.
+  
+  public func with(_ subviews: UIView...) -> Self {
+    self.add(subviews)
+    return self
+  }
+  
+}
+
+extension UIView: AddWith {}
